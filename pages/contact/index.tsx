b07a2a21f371ceb8189f7e15d7e1b1095b7329cc
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2021-11-06 14:27:48 +0100

    updating styling

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -10,7 +10,7 @@ export default function ContactPage ({}: ContactProps) {
   return (
     <LayoutHeader headTitle="Christoffer Granstedt - Contact" headDescription="Contact Christoffer Granstedt for questions, information or inquiries.">
       <div className="mx-auto pt-4 xl:w-3/4 px-2 text-xs sm:text-base">
-        <h1 className="text-3xl sm:text-5xl text-black mb-8 sm:mb-2">contact me</h1>
+        <h1 className="text-3xl sm:text-5xl text-black mb-2 sm:mb-8">contact me</h1>
         <div className="w-96">
           <p><FontAwesomeIcon icon={faEnvelope}/> <strong>E-mail:</strong> <a className="text-primary hover:text-third" href="mailto:christoffer.granstedt@outlook.com" target="_blank" rel="noreferrer">christoffer.granstedt@outlook.com</a></p>
           <p><i className="devicon-linkedin-plain colored"/> <strong>LinkedIn:</strong> <a className="text-primary hover:text-third" href="https://www.linkedin.com/in/christoffer-granstedt-17437b88" target="_blank" rel="noreferrer">Christoffer Granstedt</a></p>
