11c43cb091faeba895fc0fed1ef04306ef13a50c
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2021-11-08 21:10:17 +0100

    adding a better UI in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import { LinkDescription } from '@/types/LinkDescription'
 
@@ -8,11 +9,12 @@ interface NavbarProps {
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ links }) => {
+  const router = useRouter()
 
   return (
     <nav className="h-full">
       <ul className="text-center h-full flex justify-center items-center">
-        {links.map(link => <li className="inline-block text-white text-md md:text-lg mx-2 phone:mx-6 sm:mx-12" key={link.id}><Link href={link.link}><a className="hover:text-secondary">{link.name}</a></Link></li>)}
+        {links.map(link => <li className="inline-block text-gray-200 text-md md:text-lg mx-2 phone:mx-6 sm:mx-12" key={link.id}><Link href={link.link}><a className={` ${router.pathname === link.link ? 'text-white border-b-2 border-secondary font-semibold' : ''} hover:text-white`}>{link.name}</a></Link></li>)}
       </ul>
     </nav>
   )
