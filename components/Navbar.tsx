1ea061ad34cddb05532d090807603b094ac9340d
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2021-11-09 20:31:07 +0100

    adding scrollbar to remove jumping page when navigating

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar: React.FC<NavbarProps> = ({ links }) => {
   return (
     <nav className="h-full">
       <ul className="text-center h-full flex justify-center items-center">
-        {links.map(link => <li className="inline-block text-gray-200 text-md md:text-lg mx-2 phone:mx-6 sm:mx-12" key={link.id}><Link href={link.link}><a className={` ${router.pathname === link.link ? 'text-white border-b-2 border-secondary font-semibold' : ''} hover:text-white`}>{link.name}</a></Link></li>)}
+        {links.map(link => <li className="inline-block text-gray-200 text-md md:text-lg mx-2 phone:mx-6 sm:mx-12" key={link.id}><Link href={link.link}><a className={` ${router.pathname === link.link ? 'text-white border-b-2 border-secondary' : ''} hover:text-white tracking-wider`}>{link.name}</a></Link></li>)}
       </ul>
     </nav>
   )
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -11,10 +11,13 @@ interface ProjectsProps {
 export default function ProjectsPage ({ projects }: ProjectsProps) {
   return (
     <LayoutHeader headTitle="Christoffer Granstedt - Projects" headDescription="Some of the projects Christoffer Granstedt has been working on.">
-      <div className="mx-auto divide-y divide-indigo-600 divide-opacity-25 pt-4 px-2 text-xs sm:text-base">
+      <div className="mx-auto pt-4 px-2 text-xs sm:text-base">
         <h1 className="text-3xl sm:text-5xl text-black mb-2 sm:mb-8">projects</h1>
+        <p className="py-4"> More projects I have done will be added. I am currently working on a little bigger personal web application project in my spare time where my goal is that the site will go live at the beginning of 2022. 
+          In this project, I write everything in TypeScript. It has a GraphQL-backend with TypeGraphQL, TypeORM, and PostgreSQL. The frontend is using NextJS and Apollo. 
+          I will list this project as soon it is finished and live. </p>
         {projects.map(project => {
-          return <Card className="my-6" key={project.id}><ProjectInfo key={project.id} project={project}/></Card>
+          return <Card className="my-8" key={project.id}><ProjectInfo key={project.id} project={project}/></Card>
         })}
       </div>
     </LayoutHeader>
