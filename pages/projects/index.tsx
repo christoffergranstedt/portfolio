3bd12e9153b5e55f7746210fbebad5525d35cbea
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2022-01-31 12:53:27 +0100

    removing some text and updating other text

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -13,9 +13,6 @@ export default function ProjectsPage ({ projects }: ProjectsProps) {
     <LayoutHeader headTitle="Christoffer Granstedt - Projects" headDescription="Some of the projects Christoffer Granstedt has been working on.">
       <div className="mx-auto pt-4 px-2 text-xs sm:text-base">
         <h1 className="text-3xl sm:text-5xl text-black mb-2 sm:mb-8">projects</h1>
-        <p className="py-4"> More projects I have done will be added, please see my GitHub profile for more projects source codes. I am currently working on a little bigger personal web application project in my spare time where my goal is that the site will go live in Q1 2022. 
-          In this project, I write everything in TypeScript. It has a GraphQL-backend with TypeGraphQL, TypeORM, and PostgreSQL. The frontend is using NextJS and Apollo. 
-          I will list this project as soon it is finished and live. </p>
         {projects.map(project => {
           return <Card className="my-8" key={project.id}><ProjectInfo key={project.id} project={project}/></Card>
         })}
